16ad756f641df5e73c83d7438a1ac9291ef00605
lanyanhua <lanyanhua1024@163.com>
2020-12-01 15:50:29 +0800

    建项

---
+++ src/main/java/com/lancabbage/lancodeapi/LanCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lancabbage.lancodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanCodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanCodeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lancabbage/lancodeapi/LanCodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lancabbage.lancodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanCodeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
