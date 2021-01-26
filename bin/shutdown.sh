1fb3331f39a7341cdf58a84f0cf93cd0771cfbd2
lanyanhua <lanyanhua1024@163.com>
2021-01-26 19:06:59 +0800

    发布脚本

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -15,14 +15,14 @@
 cd `dirname $0`/../target
 target_dir=`pwd`
 
-pid=`ps ax | grep -i 'nacos.nacos' | grep ${target_dir} | grep java | grep -v grep | awk '{print $1}'`
+pid=`ps ax | grep -i 'gorgeous.gorgeous' | grep ${target_dir} | grep java | grep -v grep | awk '{print $1}'`
 if [ -z "$pid" ] ; then
-        echo "No nacosServer running."
+        echo "No gorgeousServer running."
         exit -1;
 fi
 
-echo "The nacosServer(${pid}) is running..."
+echo "The gorgeousServer(${pid}) is running..."
 
 kill ${pid}
 
-echo "Send shutdown request to nacosServer(${pid}) OK"
+echo "Send shutdown request to gorgeousServer(${pid}) OK"
--- bin/startup.sh
+++ bin/startup.sh
@@ -78,15 +78,13 @@ done
 export JAVA_HOME
 export JAVA="$JAVA_HOME/bin/java"
 export BASE_DIR=`cd $(dirname $0)/..; pwd`
-export DEFAULT_SEARCH_LOCATIONS="classpath:/,classpath:/config/,file:./,file:./config/"
-export CUSTOM_SEARCH_LOCATIONS=file:${BASE_DIR}/conf/,${DEFAULT_SEARCH_LOCATIONS}
+export CUSTOM_SEARCH_LOCATIONS=file:${BASE_DIR}/conf/application.properties
 
 #===========================================================================================
 # JVM Configuration
 #===========================================================================================
 if [[ "${MODE}" == "standalone" ]]; then
     JAVA_OPT="${JAVA_OPT} -Xms512m -Xmx512m -Xmn256m"
-    JAVA_OPT="${JAVA_OPT} -Dnacos.standalone=true"
 else
     if [[ "${EMBEDDED_STORAGE}" == "embedded" ]]; then
         JAVA_OPT="${JAVA_OPT} -DembeddedStorage=true"
@@ -97,28 +95,12 @@ else
 
 fi
 
-if [[ "${FUNCTION_MODE}" == "config" ]]; then
-    JAVA_OPT="${JAVA_OPT} -Dnacos.functionMode=config"
-elif [[ "${FUNCTION_MODE}" == "naming" ]]; then
-    JAVA_OPT="${JAVA_OPT} -Dnacos.functionMode=naming"
-fi
 
-JAVA_OPT="${JAVA_OPT} -Dnacos.member.list=${MEMBER_LIST}"
-
-JAVA_MAJOR_VERSION=$($JAVA -version 2>&1 | sed -E -n 's/.* version "([0-9]*).*$/\1/p')
-if [[ "$JAVA_MAJOR_VERSION" -ge "9" ]] ; then
-  JAVA_OPT="${JAVA_OPT} -Xlog:gc*:file=${BASE_DIR}/logs/nacos_gc.log:time,tags:filecount=10,filesize=102400"
-else
-  JAVA_OPT="${JAVA_OPT} -Djava.ext.dirs=${JAVA_HOME}/jre/lib/ext:${JAVA_HOME}/lib/ext"
-  JAVA_OPT="${JAVA_OPT} -Xloggc:${BASE_DIR}/logs/nacos_gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
-fi
 
 JAVA_OPT="${JAVA_OPT} -Dloader.path=${BASE_DIR}/plugins/health,${BASE_DIR}/plugins/cmdb"
-JAVA_OPT="${JAVA_OPT} -Dnacos.home=${BASE_DIR}"
 JAVA_OPT="${JAVA_OPT} -jar ${BASE_DIR}/target/${SERVER}.jar"
 JAVA_OPT="${JAVA_OPT} ${JAVA_OPT_EXT}"
 JAVA_OPT="${JAVA_OPT} --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
-JAVA_OPT="${JAVA_OPT} --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
 JAVA_OPT="${JAVA_OPT} --server.max-http-header-size=524288"
 
 if [ ! -d "${BASE_DIR}/logs" ]; then
@@ -139,5 +121,5 @@ if [ ! -f "${BASE_DIR}/logs/start.out" ]; then
 fi
 # start
 echo "$JAVA ${JAVA_OPT}" > ${BASE_DIR}/logs/start.out 2>&1 &
-nohup $JAVA ${JAVA_OPT} nacos.nacos >> ${BASE_DIR}/logs/start.out 2>&1 &
-echo "nacos is starting，you can check the ${BASE_DIR}/logs/start.out"
+nohup $JAVA ${JAVA_OPT} gorgeous.gorgeous >> ${BASE_DIR}/logs/start.out 2>&1 &
+echo "gorgeous is starting，you can check the ${BASE_DIR}/logs/start.out"
