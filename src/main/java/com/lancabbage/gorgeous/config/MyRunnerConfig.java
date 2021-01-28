562f694d6f308c9bc078950fe2f76e9a8c4075c3
lanyanhua <lanyanhua1024@163.com>
2021-01-28 15:47:55 +0800

    win

--- src/main/java/com/lancabbage/gorgeous/config/MyRunnerConfig.java
+++ src/main/java/com/lancabbage/gorgeous/config/MyRunnerConfig.java
@@ -1,7 +1,6 @@
 package com.lancabbage.gorgeous.config;
 
 import com.lancabbage.gorgeous.utils.doc.NotesConfigUtils;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,9 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * 运行配置
@@ -39,20 +41,20 @@ public class MyRunnerConfig implements CommandLineRunner {
 
     private void gitInfoConfig() {
         log.info("--开始检测git配置信息--");
-        Assert.notNull(gitInfoConfig.getUsername(), "git username为空，请检测application.properties");
-        Assert.notNull(gitInfoConfig.getPassword(), "git password为空，请检测application.properties");
+        Assert.isTrue(!StringUtils.hasLength(gitInfoConfig.getUsername()), "git username为空，请检测application.properties");
+        Assert.isTrue(!StringUtils.hasLength(gitInfoConfig.getPassword()), "git password为空，请检测application.properties");
     }
 
     private void datasource(String... args) throws Exception {
         log.info("--开始执行数据库初始化脚本--");
         for (String arg : args) {
-            if(arg.indexOf("--") != 0){
+            if (arg.indexOf("--") != 0) {
                 continue;
             }
             String key = arg.substring(2, arg.indexOf("="));
             String value = arg.substring(arg.indexOf("=") + 1);
             log.info("key:{},value:{}", key, value);
-            if("gorgeous-database".equals(key)){
+            if ("gorgeous-database".equals(key)) {
                 ScriptRunner runner = new ScriptRunner(dataSource.getConnection());
                 runner.setAutoCommit(true);
                 runner.setStopOnError(true);
